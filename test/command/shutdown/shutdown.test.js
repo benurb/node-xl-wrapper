dcd4828009ed9a7f1b9d0ccab5ff95d3edaeb169
Benjamin Urban <github@benjaminurban.de>
2014-11-15 11:31:59 +0100

    Add forgotten test files

---
+++ test/command/shutdown/shutdown.test.js
@@ -0,0 +1,62 @@
+var testConfig = require('../../config'),
+    expect = require('chai').expect,
+    path = require('path'),
+    XL = require('../../../lib');
+
+describe('shutdown', function() {
+    var xl;
+
+    before(function() {
+        testConfig.executorOptions.mock = require(path.join(__dirname, 'mock.js'));
+        xl = new XL(testConfig);
+    });
+
+    var successCallback = function(done) {
+        return function(err, data) {
+            if(err) {
+                return done(err);
+            }
+
+            expect(err).to.be.null();
+            expect(data).to.eql('Shutting down domain 5');
+            done();
+        };
+    };
+
+    it('should be successful when called with name property', function(done) {
+        xl.shutdown({
+            'name': 'test-domU'
+        }, successCallback(done));
+    });
+
+    it('should be successful when called with domName property', function(done) {
+        xl.shutdown({
+            'domName': 'test-domU'
+        }, successCallback(done));
+    });
+
+    it('should be successful when called with domainName property', function(done) {
+        xl.shutdown({
+            'domainName': 'test-domU'
+        }, successCallback(done));
+    });
+
+    it('should be successful when called with id property', function(done) {
+        xl.shutdown({
+            'id': '5'
+        }, successCallback(done));
+    });
+
+    it('should be successful when called with domId property', function(done) {
+        xl.shutdown({
+            'domId': '5'
+        }, successCallback(done));
+    });
+
+    it('should be successful when called with domainId property', function(done) {
+        xl.shutdown({
+            'domainId': '5'
+        }, successCallback(done));
+    });
+
+});
